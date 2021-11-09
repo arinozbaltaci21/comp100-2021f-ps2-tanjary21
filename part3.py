c54c694257120c4b978a84dbe220f93e990c9e4e
tanjary21 <79201814+tanjary21@users.noreply.github.com>
2021-11-09 09:01:52 +0000

    part3

--- part3.py
+++ part3.py
@@ -26,13 +26,13 @@ def is_right(player_answer, possible_answers):
 #print(f'possible_answers: {possible_answers}')
 round_points = 0
 #player_1_answer = input("player 1 answer: ")
-player_1_answer = input()
+player_1_answer = input().strip()
 round_points += is_right(player_1_answer,possible_answers)
 if round_points != 0:
   #print("\nplayer 1 controls\n")
   while player_1_strikes_left != 0 and len(possible_answers) != 0:
     #player_1_answer = input("player 1 answer: ")
-    player_1_answer = input()
+    player_1_answer = input().strip()
     points = is_right(player_1_answer,possible_answers)
     if points == 0:
       player_1_strikes_left -= 1
@@ -46,7 +46,7 @@ if round_points != 0:
     #print(" \nplayer 1 out of strikes. player 2, chance to steal")
     #print(f'\n{possible_answers}\n')
     #player_2_answer = input("player 2 answer: ")
-    player_2_answer = input()
+    player_2_answer = input().strip()
     points = is_right(player_2_answer, possible_answers)
     if points != 0:
       player_2_score += round_points
@@ -56,7 +56,7 @@ else:
   #print("\nplayer 2 controls")
   while player_2_strikes_left != 0 and len(possible_answers) != 0:
     #player_2_answer = input("player 2 answer: ")
-    player_2_answer = input()
+    player_2_answer = input().strip()
     points = is_right(player_2_answer,possible_answers)
     if points == 0:
       player_2_strikes_left -= 1
@@ -70,7 +70,7 @@ else:
     #print(" \nplayer 2 out of strikes. player 1, chance to steal")
     #print(f'\n{possible_answers}\n')
     #player_1_answer = input("player 1 answer: ")
-    player_1_answer = input()
+    player_1_answer = input().strip()
     points = is_right(player_1_answer, possible_answers)
     if points != 0:
       player_1_score += round_points
